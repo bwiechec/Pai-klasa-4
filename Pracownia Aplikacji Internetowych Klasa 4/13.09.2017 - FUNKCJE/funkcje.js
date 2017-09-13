81bd4ac027e4ccfbe4f8a232555af52dd1893d35
oddgold <bartekwiechec@onet.eu>
2017-09-13 18:57:06 +0200

    Zrobione zad dom

--- Pracownia Aplikacji Internetowych Klasa 4/13.09.2017 - FUNKCJE/funkcje.js
+++ Pracownia Aplikacji Internetowych Klasa 4/13.09.2017 - FUNKCJE/funkcje.js
@@ -1,10 +1,10 @@
-function witaj(imie){
+/*function witaj(imie){
     document.write('Witaj ' + imie);
 }
 
 
 var name = 'Maciej'
-witaj(name);
+witaj(name);*/
 
 /*
 var pole;
@@ -25,7 +25,7 @@ poleProstokata(a,b);
 document.write("<br> pole prostakata " + pole);
 */
 
-var a = 5, b=5, h =6;
+/*var a = 5, b=5, h =6;
 
 function poleObjetosc(a,b,h){
     var pole = a * b;
@@ -38,51 +38,68 @@ function poleObjetosc(a,b,h){
 console.log(poleObjetosc(a,b,h));
 
 var pole = poleObjetosc(a,b,h)[0];
-var objetosc = poleObjetosc(a,b,h)[1];
+var objetosc = poleObjetosc(a,b,h)[1];*/
 
 
 //document.write("<br>pole: " + pole + " objetosc: " + objetosc);
 
 
-
+/*
 var coChcesz = prompt("Chcesz pole czy objętosc prostokąta","0-pole 1-objetosc");
 if(coChcesz == 1) document.write('<br>Objetosc wynosi: ' + objetosc);
-if(coChcesz == 0) document.write('<br>Pole wynosi: ' + pole);
+if(coChcesz == 0) document.write('<br>Pole wynosi: ' + pole);*/
 
 
-/*zad. napisac skrypt obliczający obwód oraz objętość stożka.
+/*zad. napisac skrypt obliczający pole, obwód oraz objętość stożka.
 Dane użytkownik podaje z klawiatury. 
 wykorzytstaj obiekt Math. użytkownik za pomocą radio wybiera 
 co chce obliczyc dane dodaje w formularzu
 dane wyswietl w bloku strony*/
 
+var wynik;
+
+var pi = Math.PI;
+console.log(pi);
+
 var doc = document.getElementById;
 
 function zadaniePoleITP(){
     
-    var coliczyc = doc(co).value;
     
-    switch(coliczyc){
-        case 1:
-            
-            break;
-            
-            
-        case 2:
-            
-            
-            break;
-            
-        case 3:
-            
-            break;
-            
-        default:
-            document.write('Nie wybrałeś nic!!!');
-            
-    }
+    var promien = document.getElementById('r').value;
+    console.log(promien);
+    var wysokosc = document.getElementById('h').value;
+    console.log(wysokosc);
     
+    Number(promien);
+    Number(wysokosc);
+        if(co1.checked == true){
+            
+            wynik = pi * (promien*promien);
+            console.log(wynik);
+            
+        }
+            
+            
+        if(co2.checked == true){
+            
+             wynik = 2 * pi *promien;
+            console.log(wynik);
+            
+        }
+            
+        if(co3.checked == true){
+            
+             wynik = 1/3*(pi * (promien*promien) * wysokosc);
+             console.log(wynik);
+            
+         }
+            
+        else if(co1.checked != true && co2.checked != true && co3.checked != true) document.write('Nie wybrałeś nic!!!');
+            
+    console.log(wynik);
 }
+
 
 
 
