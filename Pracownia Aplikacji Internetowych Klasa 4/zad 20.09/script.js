10a600626ecc5f154ac3e9edf6e77c0932807b39
oddgold <bartekwiechec@onet.eu>
2017-09-21 08:07:10 +0200

    Zad dom dodane

---
+++ Pracownia Aplikacji Internetowych Klasa 4/zad 20.09/script.js
@@ -0,0 +1,37 @@
+var form = document.forms['form1'];
+
+
+
+var elPrzycisk = document.getElementById('wyslij');
+var elWynik = document.getElementById('wynik');
+
+elPrzycisk.onclick = function(){
+    var ilosc = form.ilosc.value;
+    var rodzaj = form.rodzaj.value;
+    console.log(ilosc);
+    console.log(rodzaj);
+    
+    if(rodzaj == '') rodzaj = '*';
+    console.log(ilosc);
+    console.log(rodzaj);
+    if(!isNaN(ilosc)){
+    var i=1;
+        var napis = "";
+    for(i;i<=ilosc;i++){
+        for(j=0;j<i;j++) {
+            napis = napis + rodzaj;
+                             }
+        napis =napis + "<br>";
+        }
+  
+    for(i;i>=0;i--){
+        for(j=0;j<i;j++) {
+            napis = napis + rodzaj;
+                             }
+        napis =napis + "<br>";
+        }
+        elWynik.innerHTML = napis;
+    }  
+    else if(ilosc == "") elWynik.textContent = "nie wybrałeś liczby gwiazdek";
+    else elWynik.textContent = "Nie wybrałeś poprawnie liczby gwiazdek";
+}
